8b113888825e0f7f56e1c6bcea1bdfce11e993d0
Matheus Leite <matheusleitecordeiro@gmail.com>
2026-09-24 02:03:50 -0300

    fix: adicionando código apagado do StrawberryCake.java

---
+++ StrawberryCake.java
@@ -0,0 +1,11 @@
+public class StrawberryCake extends Cake {
+    @Override
+    public int getCost() {
+        return 20;  // dobro de 10
+    }
+
+    @Override
+    public String getDescription() {
+        return "Strawberry cake";
+    }
+}
